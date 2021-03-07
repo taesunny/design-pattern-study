f69f0491b6273480e3e9e807e4e99698ed0ce4d6
Taesun Lee <superbsun@gmail.com>
2021-03-07 15:22:42 +0900

    add a buider example

--- src/App.java
+++ src/App.java
@@ -1,15 +1,21 @@
+import builder.ContainerConfig;
 import factorymethod.FileManager;
 import factorymethod.FileManagerFactory;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, It's Design Pattern Tester!");
+        System.out.println("");
 
         runFactoryMethodExample();
+        System.out.println("");
+
+        runBuilderExample();
+        System.out.println("");
     }
 
     private static void runFactoryMethodExample() {
-        System.out.println("--- Factory Method Example Start!");
+        System.out.println("--- Factory Method Example Start! ---");
 
         FileManager nfsFM = FileManagerFactory.getFileManager("NFS");
         nfsFM.createAFile("test-name", "test-content");
@@ -19,6 +25,22 @@ public class App {
         localFM.createAFile("test-name", "test-content");
         localFM.deleteAFile("test-name");
 
-        System.out.println("--- Factory Method Example End!");
+        System.out.println("--- Factory Method Example End! ---");
+    }
+
+    private static void runBuilderExample() {
+        System.out.println("--- Builder Example Start! ---");
+
+        System.out.println("Container Config1:");
+        ContainerConfig config1 = new ContainerConfig.Builder("name1", "ubuntu").port(80).autoRestart(true).build();
+        System.out.println("--- port: " + config1.getPort());
+        System.out.println("--- autoRestart: " + config1.isAutoRestart());
+
+        System.out.println("Container Config2:");
+        ContainerConfig config2 = new ContainerConfig.Builder("name2", "mysql").build();
+        System.out.println("--- port: " + config2.getPort());
+        System.out.println("--- autoRestart: " + config2.isAutoRestart());
+
+        System.out.println("--- Builder Example End! ---");
     }
 }
---
+++ src/builder/ContainerConfig.java
@@ -0,0 +1,51 @@
+package builder;
+
+import java.util.Map;
+
+public class ContainerConfig {
+    private final String name;
+    private final String image;
+    private final int port;
+    private final boolean autoRestart;
+
+    public static class Builder {
+        private final String name;
+        private final String image;
+        private int port;
+        private boolean autoRestart;
+
+        public Builder(String name, String image) {
+            this.name = name;
+            this.image = image;
+        }
+
+        public Builder port(int port) {
+            this.port = port;
+            return this;
+        }
+
+        public Builder autoRestart(boolean autoRestart) {
+            this.autoRestart = autoRestart;
+            return this;
+        }
+
+        public ContainerConfig build() {
+            return new ContainerConfig(this);
+        }
+    }
+
+    public ContainerConfig(Builder builder) {
+        this.name = builder.name;
+        this.image = builder.image;
+        this.port = builder.port;
+        this.autoRestart = builder.autoRestart;
+    }
+
+    public int getPort() {
+        return this.port;
+    }
+
+    public boolean isAutoRestart() {
+        return this.autoRestart;
+    }
+}
