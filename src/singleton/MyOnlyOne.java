ee80731804dbb35d31dec8e5eaa915facbdf5503
Taesun Lee <superbsun@gmail.com>
2021-03-09 00:46:28 +0900

    add example to prevent race condition of singleton

--- src/singleton/MyOnlyOne.java
+++ src/singleton/MyOnlyOne.java
@@ -3,6 +3,11 @@ package singleton;
 public class MyOnlyOne {
     private static MyOnlyOne moo = null;
 
+    // way 1 to prevent race condition
+    // private static MyOnlyOne moo = new MyOnlyOne();
+
+    // way 2 to prevent race condition
+    // public synchronized static MyOnlyOne getMyOnlyOne() {
     public static MyOnlyOne getMyOnlyOne() {
         if (moo == null) {
             moo = new MyOnlyOne();
